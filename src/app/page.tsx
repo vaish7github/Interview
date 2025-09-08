73df27a18948e6c8787a806af1019cb6e676fc2b
vaish7github <vaishnavitiwari968@gmail.com>
2025-09-08 10:54:12 +0530

    clerk auth

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,14 +1,17 @@
 import { Button } from "@/components/ui/button";
+import { SignedOut, SignInButton, SignUpButton, SignedIn, UserButton } from "@clerk/nextjs";
 import Image from "next/image";
 
 export default function Home() {
   return (
-    <div>
-      <h1>Home </h1>
-      <Button>
-        Sign In
-      </Button>
+    <div className="m-10">
+      <SignedOut>
+        <SignInButton />
+      </SignedOut>
+      <SignedIn>
+        <UserButton />
+      </SignedIn>
     </div>
-     );
+  );
 
 }
