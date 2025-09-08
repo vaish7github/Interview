3adf862a42a16bbe49109008df5fc779f138f3a5
vaish7github <vaishnavitiwari968@gmail.com>
2025-09-08 11:10:02 +0530

    users table- first convex setup

---
+++ convex/schema.ts
@@ -0,0 +1,16 @@
+// this is the schema of our db the shape of our data base
+
+import { defineSchema, defineTable } from "convex/server";
+import { v } from "convex/values";
+
+export default defineSchema({
+// this is our users table defined in the schema
+    users: defineTable({
+        name: v.string(),
+        email: v.string(),
+        image: v.optional(v.string()),
+        role: v.union(v.literal("candidate"), v.literal("interviewer")),
+        clerkId: v.string(),
+    }).index("by_clerk_id", ["clerkId"]),
+});
+// now this table is pushed in the convex 
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -4,14 +4,7 @@ import Image from "next/image";
 
 export default function Home() {
   return (
-    <div className="m-10">
-      <SignedOut>
-        <SignInButton />
-      </SignedOut>
-      <SignedIn>
-        <UserButton />
-      </SignedIn>
-    </div>
+    <div className="m-10">Home Page</div>
   );
 
 }
