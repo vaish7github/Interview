846b34b9830d6bfd09a41abbe98aa8e2b74ead4c
vaish7github <vaishnavitiwari968@gmail.com>
2025-09-08 10:42:50 +0530

    shad cn, react installation

---
+++ src/app/page.tsx
@@ -0,0 +1,14 @@
+import { Button } from "@/components/ui/button";
+import Image from "next/image";
+
+export default function Home() {
+  return (
+    <div>
+      <h1>Home </h1>
+      <Button>
+        Sign In
+      </Button>
+    </div>
+     );
+
+}
